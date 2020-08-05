908a1655caeb7f7b275030038ea5844455f1e9a2
Loutroop2107 <59732960+loutroop@users.noreply.github.com>
2020-08-05 09:50:13 +0800

    Add files via upload

---
+++ TextCassieEventbc/Configs.cs
@@ -0,0 +1,34 @@
+﻿using Exiled.API.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextCassieEventbc
+{
+    public class Configs : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public static string DecontaminationInit { get; set; } = "<size=30><color=purple>Attention, all personnel, the Light Containment Zone decontamination process will occur in t-minus 15 minutes. \nAll biological substances must be removed in order to avoid destruction.</color></size>";
+        public static string DecontaminationMinutesCount { get; set; } = "<size=30><color=purple>Danger, Light Containment zone overall decontamination in T-minus {0} Minutes.</color></size>";
+        public static string Decontamination30s { get; set; } = "<size=30><color=purple>Danger, Light Containment Zone overall decontamination in T-minus 30 seconds.\nAll checkpoint doors have been permanently opened. Please evacuate immediately.</color></size>";
+        public static string DecontaminationLockdown { get; set; } = "<size=30><color=purple>Light Containment Zone is locked down and ready for decontamination. The removal of organic substances has now begun.</color></size>";
+        public static string DecontEvent { get; set; } = "Decontamination process for Light Containment Zone has been started. SCP subject in zone will be destroyed.";
+        public static string GeneratorFinish { get; set; } = "<size=30><color=blue>{0} out of 5 generators activated.</color></size>";
+        public static string GeneratorComplete { get; set; } = "<size=30><color=blue>5 out of 5 generators activated.\nAll generators has been sucessfully engaged.\nFinalizing recontainment sequence.\nHeavy containment zone will overcharge in t-minus 1 minutes.</color></size>";
+        public static string Generatorofflight { get; set; } = "All SCP subject has been secured. SCP-079 recontainment sequence commencing.\nHeavy containment zone will overcharge in t-minus 1 minutes.";
+        public static string AlphaWarheadStart { get; set; } = "<size=30><color=purple>Alpha Warhead emergency detonation sequence engaged.\nThe underground section of the facility will be detonated in t-minus {0} seconds.</color></size>";
+        public static string AlphaWarheadResume { get; set; } = "<size=30><color=purple>Detonation sequence resumed. t-minus {0} seconds.</color></size>";
+        public static string AlphaWarheadCancel { get; set; } = "Detonation cancelled. Restarting systems.";
+        public static string SCPDeathTesla { get; set; } = "{0} successfully terminated by automatic security system.";
+        public static string SCPDeathWarhead { get; set; } = "{0} terminated by alpha warhead.";
+        public static string SCPDeathDecont { get; set; } = "{0} lost in decontamination sequence.";
+        public static string SCPDeathTerminated { get; set; } = "{0} terminated by {1}.{-1}";
+        public static string SCPDeathContainedMTF { get; set; } = "{0} contained successfully. Containment unit:{1}.{-2}";
+        public static string SCPDeathUnknown { get; set; } = "{0} successfully terminated. Termination cause unspecified.{-2}";
+        public static string MTFRespawnSCPS { get; set; } = "<size=20><color=blue>Mobile Task Force Unit, Epsilon-11, designated, '{0}', has entered the facility.\nAll remaining personnel are advised to proceed with standard evacuation protocols until an MTF squad reaches your destination.\nAwaiting recontainment of: {1} SCP subject.</color></size>";
+        public static string MTFRespawnNOSCP { get; set; } = "<size=20><color=blue>Mobile Task Force Unit, Epsilon-11, designated, '{0}', has entered the facility.\nAll remaining personnel are advised to proceed with standard evacuation protocols, until MTF squad has reached your destination.\nSubstantial threat to safety is within the facility -- Exercise caution.</color></size>";
+    }   
+         
+}
---
+++ TextCassieEventbc/EventHandlers.cs
@@ -0,0 +1,150 @@
+﻿using Exiled.API.Features;
+using Exiled.Events;
+using Exiled.Events.EventArgs;
+using Respawning;
+using Respawning.NamingRules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextCassieEventbc
+{
+    public class EventHandlers
+    {
+        private Plugin plugin;
+        public EventHandlers(Plugin plugin) => this.plugin = plugin;
+        public void AnnouncingDecontamination(AnnouncingDecontaminationEventArgs ev)
+        {
+            switch (ev.Id)
+            {
+                case 0:
+                    {
+                        Map.Broadcast(15, Configs.DecontaminationInit);
+                        break;
+                    }
+                case 1:
+                    {
+                        Map.Broadcast(15, Configs.DecontaminationMinutesCount.Replace("{0}", "10"));
+                        break;
+                    }
+                case 2:
+                    {
+                        Map.Broadcast(15, Configs.DecontaminationMinutesCount.Replace("{0}", "5"));
+                        break;
+                    }
+                case 3:
+                    {
+                        Map.Broadcast(15, Configs.DecontaminationMinutesCount.Replace("{0}", "1"));
+                        break;
+                    }
+                case 4:
+                    {
+                        Map.Broadcast(15, Configs.Decontamination30s);
+                        break;
+                    }
+            }
+        }
+        public void OnDecontaminating(DecontaminatingEventArgs ev)
+        {
+            Map.Broadcast(15, Configs.DecontaminationLockdown);
+        }
+
+        public void OnGeneratorActivated(GeneratorActivatedEventArgs ev)
+        {
+            int curgen = Generator079.mainGenerator.NetworktotalVoltage + 1;
+            if (curgen < 5)
+            {
+                Map.Broadcast(10, Configs.GeneratorFinish.Replace("{0}", curgen.ToString()));
+            }
+            else
+            {
+                Map.Broadcast(20, Configs.GeneratorComplete);
+            }
+        }
+        public void AlphaStart(StartingEventArgs ev)
+        {
+            bool isresumed = AlphaWarheadController._resumeScenario != -1;
+            double left = isresumed ? AlphaWarheadController.Host.timeToDetonation : AlphaWarheadController.Host.timeToDetonation - 4;
+            double count = Math.Truncate(left / 10.0) * 10.0;
+            if (!isresumed)
+            {
+                Map.Broadcast(15, Configs.AlphaWarheadStart.Replace("{0}", count.ToString()));
+            }
+            else
+            {
+                Map.Broadcast(10, Configs.AlphaWarheadResume.Replace("{0}", count.ToString()));
+            }
+        }
+        public void AlohaStop(StoppingEventArgs ev)
+        {
+            Map.Broadcast(7, Configs.AlphaWarheadCancel);
+        }
+        public void Died(DiedEventArgs ev)
+        {
+            var damagetypes = ev.HitInformations.GetDamageType();
+            var targetrole = ev.Target.ReferenceHub.characterClassManager._prevId;
+            string fullname = CharacterClassManager._staticClasses.Get(targetrole).fullName;
+            if (ev.Target.Team == Team.SCP)
+            {
+                if (damagetypes == DamageTypes.Tesla)
+                {
+                    Map.Broadcast(10, Configs.SCPDeathTesla.Replace("{0}", fullname));
+                }
+                if (damagetypes == DamageTypes.Nuke)
+                {
+                    Map.Broadcast(10, Configs.SCPDeathWarhead.Replace("{0}", fullname));
+                }
+                if (damagetypes == DamageTypes.Decont)
+                {
+                    Map.Broadcast(10, Configs.SCPDeathDecont.Replace("{0}", fullname));
+                }
+            }
+            else if (ev.Target.Team == Team.SCP)
+            {
+                if (ev.Killer.Team == Team.CDP)
+                {
+                     Map.Broadcast(10, Configs.SCPDeathTerminated.Replace("{0}", fullname).Replace("{1}", "Class-D Personnel"));
+                }
+                else if (ev.Killer.Team == Team.CDP && Player.List.Any(x => x.Role == RoleType.Scp079) && Player.List.Count(x => x.Team == Team.SCP) == 1
+                    && Generator079.mainGenerator.totalVoltage < 4 && !Generator079.mainGenerator.forcedOvercharge && damagetypes != DamageTypes.Nuke)
+                {
+                    Map.Broadcast(10, Configs.SCPDeathTerminated.Replace("{0}", fullname).Replace("{1}", "Class-D Personnel").Replace("{-2}", "All SCP subject has been secured.SCP - 079 recontainment sequence commencing.\nHeavy containment zone will overcharge in t - minus 1 minutes."));
+                }
+                else if (ev.Killer.Team == Team.CHI)
+                {
+                    Map.Broadcast(10, Configs.SCPDeathTerminated.Replace("{0}", fullname).Replace("{1}", "Chaos Insurgency"));
+                }
+                else if (ev.Killer.Team == Team.CHI && Player.List.Any(x => x.Role == RoleType.Scp079) && Player.List.Count(x => x.Team == Team.SCP) == 1
+                    && Generator079.mainGenerator.totalVoltage < 4 && !Generator079.mainGenerator.forcedOvercharge && damagetypes != DamageTypes.Nuke)
+                {
+                    Map.Broadcast(10, Configs.SCPDeathTerminated.Replace("{0}", fullname).Replace("{1}", "Chaos Insurgency"));
+                }
+                else if (ev.Killer.Team == Team.RSC)
+                {
+                    Map.Broadcast(10, Configs.SCPDeathTerminated.Replace("{0}", fullname).Replace("{1}", "Scientist"));
+                }
+                else if (ev.Killer.Team == Team.RSC && Player.List.Any(x => x.Role == RoleType.Scp079) && Player.List.Count(x => x.Team == Team.SCP) == 1
+                    && Generator079.mainGenerator.totalVoltage < 4 && !Generator079.mainGenerator.forcedOvercharge && damagetypes != DamageTypes.Nuke)
+                {
+                    Map.Broadcast(10, Configs.SCPDeathTerminated.Replace("{0}", fullname).Replace("{1}", "Scientist"));
+                }
+                else if (ev.Killer.Team == Team.MTF)
+                {
+                    Map.Broadcast(10, Configs.SCPDeathContainedMTF.Replace("{0}", fullname).Replace("{1}", ev.Killer.ReferenceHub.characterClassManager.CurUnitName));
+                }
+                else if (ev.Killer.Team == Team.MTF && Player.List.Any(x => x.Role == RoleType.Scp079) && Player.List.Count(x => x.Team == Team.SCP) == 1
+                    && Generator079.mainGenerator.totalVoltage < 4 && !Generator079.mainGenerator.forcedOvercharge && damagetypes != DamageTypes.Nuke)
+                {
+                    Map.Broadcast(10, Configs.SCPDeathContainedMTF.Replace("{0}", fullname).Replace("{1}", ev.Killer.ReferenceHub.characterClassManager.CurUnitName));
+                }
+                else
+                {
+                    Map.Broadcast(10, Configs.SCPDeathUnknown.Replace("{0}", fullname));
+                }
+            }
+
+        }
+    }
+}
